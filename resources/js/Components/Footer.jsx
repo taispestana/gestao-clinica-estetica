a5daaf8f524dc1947948d5a45dd9ea8da8050e52
Claudia Machado <claudia.machado.webdesign@gmail.com>
2026-01-13 22:10:05 +0000

    desenvolvimento footer

---
+++ resources/js/Components/Footer.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="fixed-bottom w-100 py-4" style={{ backgroundColor: '#1E2D24' }}>
+        <div className="container text-center">
+            <p className="text-white small fw-light mb-0">
+            Design com <span className="text-danger">❤️</span> por Equipa Vértice
+            </p>
+        </div>
+        </footer>
+    );
+    };
+
+export default Footer;
