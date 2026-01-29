986ccc22130ebc0cf05af346d5bbb0c09be8c937
anthony <anthonydcm1@gmail.com>
2026-01-29 13:03:45 +0000

    apaguei conponentes desnecesarios, e modei alguns comentarios arrumei a web.php

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\TratamentoController;
 use App\Http\Controllers\AgendamentoController;
 use App\Http\Controllers\AnamneseController;
-
+use App\Http\Controllers\MensagensController;
 use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
@@ -21,32 +21,33 @@ Route::get('/', function () {
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/clientes', [UserController::class, 'index'])->middleware(['auth', 'verified'])->name('clientes');
 
+Route::get('/clientes', [UserController::class, 'index'])->middleware(['auth', 'verified'])->name('clientes');
 Route::get('/clientes/{id}', [UserController::class, 'show'])->middleware(['auth', 'verified'])->name('clientes.show');
 Route::put('/clientes/{id}', [UserController::class, 'update'])->middleware(['auth', 'verified'])->name('clientes.update');
-
+Route::post('/anamneses', [AnamneseController::class, 'store'])->middleware(['auth', 'verified'])->name('anamneses.store');
 
 
 Route::get('/agendamentos', [AgendamentoController::class, 'index'])->middleware(['auth', 'verified'])->name('agendamentos');
 Route::post('/agendamentos', [AgendamentoController::class, 'store'])->middleware(['auth', 'verified'])->name('agendamentos.store');
 Route::delete('/agendamentos/{id}', [AgendamentoController::class, 'destroy'])->middleware(['auth', 'verified'])->name('agendamentos.destroy');
 
-Route::post('/anamneses', [AnamneseController::class, 'store'])->middleware(['auth', 'verified'])->name('anamneses.store');
+
 
 Route::get('/tratamentos', [TratamentoController::class, 'index'])->middleware(['auth', 'verified'])->name('tratamentos');
 Route::post('/storeTratamento', [TratamentoController::class, 'storeTratamento'])->name('tratamentos.store');
 Route::put('/tratamentos/{id}', [TratamentoController::class, 'update'])->middleware(['auth', 'verified'])->name('tratamentos.update');
 Route::delete('/tratamentos/{id}', [TratamentoController::class, 'destroy'])->middleware(['auth', 'verified'])->name('tratamentos.destroy');
 
-use App\Http\Controllers\MensagensController;
 
 Route::get('/mensagens', [MensagensController::class, 'index'])->middleware(['auth', 'verified'])->name('mensagens');
+
 
 Route::get('/estoque', [ProdutoController::class, 'index'])->middleware(['auth', 'verified'])->name('estoque');
 Route::post('/storeProduto', [ProdutoController::class, 'storeProduto'])->name('estoque.storeProduto');
 Route::put('/estoque/{id}', [ProdutoController::class, 'update'])->middleware(['auth', 'verified'])->name('estoque.update');
 Route::delete('/estoque/{id}', [ProdutoController::class, 'destroy'])->middleware(['auth', 'verified'])->name('estoque.destroy');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
